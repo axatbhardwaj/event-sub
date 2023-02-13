7701bd5fedcaba7ce741776c4c1c60d1f9442b2c
abhardwaj <abhrdwaj@infrablok.com>
2023-02-13 10:45:52 +0530

    added node switcher

---
+++ scripts/nodeSwitcher.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+
+const switcher = (number) => {
+
+    let Provider;
+    let decimals = number.length-1;
+    number = parseInt(number);
+    let choice = number % (10);
+   // console.log(choice);
+
+    switch (choice)
+    {
+        case 0:
+            Provider = process.env.node5;
+             break;
+        case 1:
+            Provider = process.env.node1;
+             break;
+        case 2:
+            Provider = process.env.node2;
+             break;
+        case 3:
+            Provider = process.env.node3;
+             break;
+        case 4:
+            Provider = process.env.node4;
+             break;
+        case 5:
+            Provider = process.env.node5;
+            break;
+        case 6:
+            Provider = process.env.node1;
+            break;
+        case 7:
+            Provider = process.env.node2;
+            break;
+        case 8:
+            Provider = process.env.node3;
+            break;
+        case 9:
+            Provider = process.env.node4;
+             break;
+    }
+    console.log(Provider);
+}
+
+function looper(){
+
+    for (var i = 0; i <= 100; i++)
+    {
+        i = i.toString();
+        switcher(i);
+        }
+}
+
+looper();
